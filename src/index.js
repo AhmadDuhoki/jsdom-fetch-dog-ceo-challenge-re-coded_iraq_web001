6c14d9a4343a78ff5fa94e9683b615c9e38e903b
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-03 12:54:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+document.addEventListener("DOMContentLoaded", function(){
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  const imgContainer = document.getElementById("dog-image-container");
+  const breedDropDown = document.getElementById("breed-dropdown");
+  const dogBreedsContainer = document.getElementById("dog-breeds");
+  
+  fetch(imgUrl)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    showDogsImg(json);
+  });
+  
+  fetch(breedUrl)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    showDogsBreed(json);
+  });
+  
+  function setRandomColor(e){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let colorized = "#" + randomColor;
+    let elementTarget = e.target.style.color = colorized;
+    return elementTarget;
+  }
+  
+  breedDropDown.addEventListener("change", (e) => {
+    let breedLis = document.getElementsByClassName("breed-list-item");
+    let select = e.target.value;
+    for (let li of breedLis) {
+      li.style.display = "block";
+    }
+    for (let li of breedLis) {
+      if (select === "all") {
+        li.style.display = "block";
+      } else {
+        li.innerText.charAt(0) === select
+          ? (li.style.display = "block")
+          : (li.style.display = "none");
+      }
+    }
+  });
+  
+  function showDogsImg(dogsImg){
+    for(const index of dogsImg.message){
+      imgContainer.insertAdjacentHTML("beforeend", `<img src="${index}" width="400">`);
+    }
+  }
+  
+  function showDogsBreed(dogsBreed){
+    for(const dBreed in dogsBreed.message){
+      let li = document.createElement('li');
+      li.innerText = dBreed;
+      dogBreedsContainer.appendChild(li);
+      li.addEventListener('click', setRandomColor);
+    }
+  }
+  
+});
